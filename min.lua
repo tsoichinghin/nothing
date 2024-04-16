6ef0a0063623beadc600398aae4080dcd3995287
tsoichinghin <157337442+tsoichinghin@users.noreply.github.com>
2024-04-16 23:03:48 +0800

    Update min.lua

--- min.lua
+++ min.lua
@@ -1,8 +1,4 @@
-function on_window_open(event)
-    if (get_application_name() == "PacketShare") or (get_window_name() == "PacketShare") or (get_window_name() == "Wine System Tray") then
-        debug_print("PacketShare window detected. Minimizing...")
-        minimize();
-    end
+if (get_application_name() == "PacketShare") or (get_window_name() == "PacketShare") or (get_window_name() == "Wine System Tray") then
+    debug_print("Lua said：PacketShare window detected. Minimizing...")
+    minimize();
 end
-
-add_signal_handler("window_open", on_window_open)
