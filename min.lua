a2069635872fed7f981c79cf2c578f09a0d72635
tsoichinghin <157337442+tsoichinghin@users.noreply.github.com>
2024-04-16 19:10:50 +0800

    Update and rename auto_no_minimize.lua to min.lua

--- min.lua
+++ min.lua
@@ -1,6 +1,6 @@
-debug_print("Checking window: " .. get_window_name())
-
+local socket = require("socket")
 if (get_application_name() == "PacketShare") then
     debug_print("PacketShare window detected. Minimizing...")
+    socket.sleep(25)
     minimize();
 end
