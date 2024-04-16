396eba50a624613d846cf93900056ee944b6b7ab
tsoichinghin <157337442+tsoichinghin@users.noreply.github.com>
2024-04-16 23:04:29 +0800

    Create min.sh

---
+++ min.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Enter the program!"
+
+while true; do
+    window_id=$(xdotool search --name "PacketShare")
+    if [ -n "$window_id" ]; then
+        packetshare_pid=$(pgrep -f "PacketShare.exe" || true)
+        if [ -n "$packetshare_pid" ]; then
+            cpulimit -p "$packetshare_pid" -l 10 &
+            echo "cpulimit command executed for PacketShare.exe with PID $packetshare_pid"
+        else
+            echo "PacketShare.exe not running"
+        fi
+        devilspie2 --file min.lua
+        echo "PacketShare window minimized."
+    else
+        echo "PacketShare window not found."
+    fi
+    sleep 5
+done
