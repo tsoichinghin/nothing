089baca5f3a0ffd5688f3bf5452ab0c7f69c3ac1
tsoichinghin <157337442+tsoichinghin@users.noreply.github.com>
2024-04-16 22:33:49 +0800

    Update min.lua

--- min.lua
+++ min.lua
@@ -1,6 +1,8 @@
-local socket = require("socket")
-if (get_application_name() == "PacketShare") then
-    debug_print("PacketShare window detected. Minimizing...")
-    socket.sleep(25)
-    minimize();
+function on_window_open(event)
+    if (get_application_name() == "PacketShare") or (get_window_name() == "PacketShare") or (get_window_name() == "Wine System Tray") then
+        debug_print("PacketShare window detected. Minimizing...")
+        minimize();
+    end
 end
+
+add_signal_handler("window_open", on_window_open)
