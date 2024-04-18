b478b66c7ae1c945880182d72ad912915213df9a
tsoichinghin <157337442+tsoichinghin@users.noreply.github.com>
2024-04-19 01:33:26 +0800

    Update min.sh

--- min.sh
+++ min.sh
@@ -18,20 +18,42 @@ while true; do
         else
             echo "PacketShare window not found."
         fi
-        packetshare_cpu_usage=$(ps -p "$packetshare_pid" -o %cpu | tail -n 1 | awk '{printf "%d", $1}')
-        echo "cpu usage is $packetshare_cpu_usage"
-        if [ "$packetshare_cpu_usage" -gt 10 ]; then
-            echo "CPU usage greater than 10 after minimize or the window even not found. It means doesn't minimize or error."
+        cpu_usage_history=()
+        for i in {1..10}; do
+            packetshare_cpu_usage=$(ps -p "$packetshare_pid" -o %cpu | tail -n 1 | awk '{printf "%d", $1}')
+            echo "CPU usage: $packetshare_cpu_usage"
+            cpu_usage_history+=("$packetshare_cpu_usage")
+            if [ ${#cpu_usage_history[@]} -gt 10 ]; then
+                cpu_usage_history=("${cpu_usage_history[@]:1}")
+            fi
+            sleep 1
+        done
+        check_cpu_usage_below_10() {
+            echo "CPU usage histor: $cpu_usage_history"
+            for usage in "${cpu_usage_history[@]}"; do
+                if (( usage < 10 )); then
+                    return 0
+                fi
+            done
+            return 1
+        }
+        if check_cpu_usage_below_10; then
+            echo "There was at least one second in the past 10 seconds when CPU usage was below 10%."
+            echo "PacketShare.exe CPU usage is normal."
+        else
+            echo "CPU usage was not below 10% in the past 10 seconds."
+            echo "CPU usage greater than 10% after minimize or the window even not found. It means doesn't minimize or error."
             echo "PacketShare.exe terminating..."
             kill "$packetshare_pid"
+            sleep 1
             wine ~/.wine/drive_c/Program\ Files/PacketShare/PacketShare.exe &
+            sleep 30
             echo "PacketShare.exe restarted."
-        else
-            echo "PacketShare.exe CPU usage is normal."
         fi
     else
         echo "PacketShare.exe not running. Restarting..."
         wine ~/.wine/drive_c/Program\ Files/PacketShare/PacketShare.exe &
+        sleep 30
     fi
     sleep 2
 done
