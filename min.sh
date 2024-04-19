e96f75886d1a41d3b47b0ebb37664dc8b4dce6c6
tsoichinghin <157337442+tsoichinghin@users.noreply.github.com>
2024-04-19 14:29:40 +0800

    Update min.sh

--- min.sh
+++ min.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+echo "Enter the program!"
+
+while true; do
+    packetshare_pid=$(pidof "PacketShare.exe")
+    window_id=$(xdotool search --name "PacketShare")
+    if [ -n "$packetshare_pid" ]; then
+        if [ -n "$window_id" ]; then
+            pkill -9 -f cpulimit
+            cpulimit -p "$packetshare_pid" -l 10 &
+            echo "cpulimit command executed for PacketShare.exe with PID $packetshare_pid"
+            devilspie2 /home/tch/.config/devilspie2/min.lua &
+            devilspie2_pid=$!
+            sleep 30
+            kill "$devilspie2_pid"
+            echo "PacketShare window minimized."
+        else
+            echo "PacketShare window not found."
+        fi
+    else
+        echo "PacketShare.exe not running. Restarting..."
+        wine ~/.wine/drive_c/Program\ Files/PacketShare/PacketShare.exe &
+        packetshare_pid=$(pidof "PacketShare.exe")
+        cpulimit -p "$packetshare_pid" -l 10 &
+        sleep 30
+    fi
+    sleep 2
+done
