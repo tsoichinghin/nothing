fe673774b02482ebac7d5007f757fe6181b3ca83
tsoichinghin <157337442+tsoichinghin@users.noreply.github.com>
2024-04-19 13:37:41 +0800

    Update min.sh

--- min.sh
+++ min.sh
@@ -2,6 +2,16 @@
 
 echo "Enter the program!"
 
+function check_cpu_usage_below_10 {
+    echo "CPU usage history: ${cpu_usage_history[@]}"
+    for usage in "${cpu_usage_history[@]}"; do
+        if (( usage < 10 )); then
+            return 0
+        fi
+    done
+    return 1
+}
+
 while true; do
     packetshare_pid=$(pidof PacketShare.exe)
     window_id=$(xdotool search --name "PacketShare")
@@ -29,15 +39,11 @@ while true; do
             fi
             sleep 1
         done
-        while true; do
-            echo "CPU usage history: ${cpu_usage_history[@]}"
-            for usage in "${cpu_usage_history[@]}"; do
-                if [ "$usage" -lt 10 ]; then
-                    echo "There was at least one second in the past 10 seconds when CPU usage was below 10%."
-                    echo "PacketShare.exe CPU usage is normal."
-                    break
-                fi
-            done
+        check_cpu_usage_below_10
+        if check_cpu_usage_below_10; then
+            echo "There was at least one second in the past 10 seconds when CPU usage was below 10%."
+            echo "PacketShare.exe CPU usage is normal."
+        else
             echo "CPU usage was not below 10% in the past 10 seconds."
             echo "CPU usage greater than 10% after minimize or the window even not found. It means doesn't minimize or error."
             echo "PacketShare.exe terminating..."
@@ -48,8 +54,7 @@ while true; do
             cpulimit -p "$packetshare_pid" -l 10 &
             sleep 30
             echo "PacketShare.exe restarted."
-            break
-        done
+        fi
     else
         echo "PacketShare.exe not running. Restarting..."
         wine ~/.wine/drive_c/Program\ Files/PacketShare/PacketShare.exe &
