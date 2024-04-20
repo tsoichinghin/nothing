06fb6fa8d7f158232d848d6df043d65bef36dcc6
tsoichinghin <157337442+tsoichinghin@users.noreply.github.com>
2024-04-20 10:10:15 +0800

    Update min.lua

--- min.lua
+++ min.lua
@@ -1,6 +1,3 @@
 if (get_application_name() == "PacketShare") or (get_window_name() == "PacketShare") then
-    maximize();
-end
-if (get_application_name() == "PacketShare") or (get_window_name() == "PacketShare") then
     minimize();
 end
