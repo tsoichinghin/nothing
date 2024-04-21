fe3dfb5a78f1f2e99747b32cb3382bc430761043
tsoichinghin <157337442+tsoichinghin@users.noreply.github.com>
2024-04-21 19:03:14 +0800

    Add files via upload

---
+++ cps.py
@@ -0,0 +1,16 @@
+import pyautogui
+import time
+
+def close_app():
+    while True:
+        pyautogui.leftClick(1208, 373)
+        print("Withdrawn closed")
+        time.sleep(5)
+        pyautogui.leftClick(1406, 260)
+        print("PacketShare closed")
+        time.sleep(5)
+        pyautogui.leftClick(145,45)
+        print("Wine closed")
+        time.sleep(5)
+
+close_app()
