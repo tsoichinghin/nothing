15ecd68fe00fba42cce5480908951f4c8b1d5889
tsoichinghin <157337442+tsoichinghin@users.noreply.github.com>
2024-04-21 19:10:29 +0800

    Update cps.py

--- cps.py
+++ cps.py
@@ -4,13 +4,16 @@ import time
 def close_app():
     while True:
         pyautogui.leftClick(1208, 373)
-        print("Withdrawn closed")
+        print("Withdrawn closed.")
         time.sleep(5)
         pyautogui.leftClick(1406, 260)
-        print("PacketShare closed")
+        print("PacketShare closed.")
         time.sleep(5)
-        pyautogui.leftClick(145,45)
-        print("Wine closed")
+        pyautogui.leftClick(145, 45)
+        print("Wine closed.")
+        time.sleep(5)
+        pyautogui.leftClick(1264, 475)
+        print("Software updater closed.")
         time.sleep(5)
 
 close_app()
