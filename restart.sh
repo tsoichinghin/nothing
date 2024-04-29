99c2f4d4cde01d2dd8ba4b25d927ffc552635729
tsoichinghin <157337442+tsoichinghin@users.noreply.github.com>
2024-04-29 10:13:28 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 while true; do
-    packetshare_pid=$(pidof "PacketShare.exe")
-    if [ -n "$packetshare_pid" ]; then
-        echo "PacketShare.exe were running."
-    else
-        echo "PacketShare.exe not running. Restarting..."
-        wine ~/.wine/drive_c/Program\ Files/PacketShare/PacketShare.exe &
-        echo "First restarting."
-        sleep 30
-        pkill -9 -f PacketShare.exe
+    counter=0
+    for i in {1..500}; do
+        counter=$((counter + 1))
+        echo "Times $counter"
+        packetshare_pid=$(pidof "PacketShare.exe")
+        if [ -n "$packetshare_pid" ]; then
+            echo "PacketShare.exe were running."
+        else
+            echo "PacketShare.exe not running. Restarting..."
+            wine ~/.wine/drive_c/Program\ Files/PacketShare/PacketShare.exe &
+            sleep 30
+            python3 ~/check.py
+            echo "PacketShare.exe restarted."
+        fi
         sleep 10
-        wine ~/.wine/drive_c/Program\ Files/PacketShare/PacketShare.exe &
-        echo "Second restarting."
-        sleep 30
-        echo "PacketShare.exe restarted."
-    fi
-    sleep 10
+    done
+    python3 ~/check.py
 done
