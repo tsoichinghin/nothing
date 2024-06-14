d82efaefc09f41e481659618e753a0c4103b1a3c
tsoichinghin <157337442+tsoichinghin@users.noreply.github.com>
2024-06-14 14:53:25 +0800

    Update monitor_feelingsurf.sh

--- monitor_feelingsurf.sh
+++ monitor_feelingsurf.sh
@@ -3,7 +3,7 @@
 CGROUP_NAME="feeling_surf_viewer"
 
 while true; do
-    PID=$(pidof FeelingSurfView)
+    PID=$(pgrep FeelingSurfView)
     if [ -n "$PID" ]; then
         for PID in $PIDS; do
             echo "$PID" | sudo tee -a "/sys/fs/cgroup/cpu/${CGROUP_NAME}/tasks"
