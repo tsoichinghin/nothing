04b923fe4e12e8299949b3dc101d9143748c7a14
tsoichinghin <157337442+tsoichinghin@users.noreply.github.com>
2024-06-14 16:12:37 +0800

    Update monitor_feelingsurf.sh

--- monitor_feelingsurf.sh
+++ monitor_feelingsurf.sh
@@ -8,18 +8,10 @@ fi
 CGROUP_NAME="feeling_surf_viewer"
 CGROUP_PATH="/sys/fs/cgroup/cpu/${CGROUP_NAME}"
 
-sudo mkdir -p "$CGROUP_PATH"
-
 while true; do
     PIDS=$(pgrep FeelingSurfView)
     if [ -n "$PIDS" ]; then
-        echo "PIDs found: $PIDS"
-        for PID in $PIDS; do
-            if ! grep -q "$PID" "$CGROUP_PATH/tasks"; then
-                echo "Writing PID $PID to $CGROUP_PATH/tasks"
-                echo "$PID" | sudo tee "$CGROUP_PATH/tasks"
-            fi
-        done
+        echo "$PIDS" | sudo tee "$CGROUP_PATH/tasks"
     else
         echo "FeelingSurfViewer process not found"
     fi
