16b5dfc2c8d68a9dcff43f3edea7da669b2aaf46
tsoichinghin <157337442+tsoichinghin@users.noreply.github.com>
2024-06-14 11:33:48 +0800

    Update monitor_feelingsurf.sh

--- monitor_feelingsurf.sh
+++ monitor_feelingsurf.sh
@@ -6,7 +6,6 @@ while true; do
     PID=$(pgrep FeelingSurfView)
 
     if [ -n "$PID" ]; then
-        echo "Found FeelingSurfViewer process with PID: $PID"
         for PID in $PIDS; do
             echo "$PID" > "/sys/fs/cgroup/cpu/${CGROUP_NAME}/tasks"
         done
