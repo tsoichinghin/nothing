58858342f5444b4f46c08c5899188b2403719da9
tsoichinghin <157337442+tsoichinghin@users.noreply.github.com>
2024-06-14 15:46:28 +0800

    Update monitor_feelingsurf.sh

--- monitor_feelingsurf.sh
+++ monitor_feelingsurf.sh
@@ -5,7 +5,9 @@ CGROUP_NAME="feeling_surf_viewer"
 while true; do
     PID=$(pgrep FeelingSurfView)
     if [ -n "$PID" ]; then
-        echo "$PID" | sudo tee -a "/sys/fs/cgroup/cpu/${CGROUP_NAME}/tasks"
+        for PID in $PIDS; do
+            echo "$PID" ｜ sudo tee -a "/sys/fs/cgroup/cpu/${CGROUP_NAME}/tasks"
+        done
     else
         echo "FeelingSurfViewer process not found"
     fi
