8f951359ce285c4bdb41491bd9a36902c5ed72f9
tsoichinghin <157337442+tsoichinghin@users.noreply.github.com>
2024-06-18 23:52:06 +0800

    Update monitor_bighits.sh

--- monitor_bighits.sh
+++ monitor_bighits.sh
@@ -11,13 +11,13 @@ CGROUP_PATH="/sys/fs/cgroup/cpu/${CGROUP_NAME}"
 while true; do
     render_pids=$(pgrep BigHits4URender)
     viewer_pids=$(pgrep BigHits4UViewer)
-    if [ -n "$render_pids" ] or [ -n "$viewer_pids" ]; then
+    if [ -n "$render_pids" ] || [ -n "$viewer_pids" ]; then
         sudo truncate -s 0 "$CGROUP_PATH/tasks"
         for render_pid in $render_pids; do
             echo "$render_pid" | sudo tee -a "$CGROUP_PATH/tasks"
         done
         for viewer_pid in $viewer_pids; do
-            echo "$viewer_pids" | sudo tee -a "$CGROUP_PATH/tasks"
+            echo "$viewer_pid" | sudo tee -a "$CGROUP_PATH/tasks"
         done
     else
         echo "BitHits process not found"
