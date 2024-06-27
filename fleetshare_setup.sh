a41a4dcc332b380d8bfec09c0a08d917e9bf8fb0
tsoichinghin <157337442+tsoichinghin@users.noreply.github.com>
2024-06-27 21:54:33 +0800

    Update fleetshare_setup.sh

--- fleetshare_setup.sh
+++ fleetshare_setup.sh
@@ -3,14 +3,14 @@
 tmux new-session -d -s edit_session 'fleetshare configure'
 sleep 5  
 tmux send-keys -t edit_session '2d881781-20c2-43a8-91f4-6f6f7bfedf0a' C-m
-sleep 2
+sleep 5
 IP=$(hostname -I | awk '{print $1}')
 tmux send-keys -t edit_session "$IP" C-m
-sleep 2
+sleep 3
 tmux send-keys -t edit_session C-o
-sleep 2
+sleep 3
 tmux send-keys -t edit_session C-m
-sleep 2
+sleep 3
 tmux send-keys -t edit_session C-x 
-sleep 2
+sleep 3
 tmux kill-session -t edit_session
