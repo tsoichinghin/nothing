28f4a32bcc2201f560e71c659ec307c5b9da250b
tsoichinghin <157337442+tsoichinghin@users.noreply.github.com>
2024-12-01 11:50:08 +0800

    Update packetshare_pid.py

--- packetshare_pid.py
+++ packetshare_pid.py
@@ -21,7 +21,7 @@ def check_packetshare():
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/rdp':
+        if self.path == '/packetshare':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
