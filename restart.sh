00c07a308eaf08fea073222b297ce9e0db5c7c82
tsoichinghin <157337442+tsoichinghin@users.noreply.github.com>
2024-12-01 10:10:35 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+SMTP_SERVER="smtp.office365.com"
+SMTP_PORT=587
+EMAIL_FROM="tsoivm@outlook.com"
+EMAIL_TO="tsoichinghin@gmail.com"
+SUBJECT="$(hostname) PacketShare.exe Terminated"
+
 while true; do
     counter=0
     for i in {1..500}; do
@@ -8,15 +14,18 @@ while true; do
         packetshare_pid=$(pidof "PacketShare.exe")
         if [ -n "$packetshare_pid" ]; then
             echo "PacketShare.exe were running."
-        else
-            echo "PacketShare.exe not running. Restarting..."
-            wine ~/.wine/drive_c/Program\ Files/PacketShare/PacketShare.exe &
-            sleep 90
-            python3 ~/check.py
-            echo "PacketShare.exe restarted."
+            cpu_usage=$(top -b -n 1 -p "$packetshare_pid" | awk 'NR>7 {print $9}')
+            cpu_usage=${cpu_usage%%.*}
+            if [ "$cpu_usage" -gt 30 ]; then
+                echo "CPU usage is $cpu_usage%. Terminating PacketShare.exe."
+                pkill -9 "PacketShare.exe"
+                echo -e "Subject:$SUBJECT\n\nPacketShare.exe was terminated due to high CPU usage ($cpu_usage%)." | \
+                    msmtp --host=$SMTP_SERVER --port=$SMTP_PORT --auth=on --user=$EMAIL_FROM --passwordeval="echo $EMAIL_PASSWORD" --tls=on $EMAIL_TO
+            fi
+        else:
+            echo -e "Subject:$SUBJECT\n\nPacketShare.exe was terminated due to high CPU usage ($cpu_usage%)." | \
+                msmtp --host=$SMTP_SERVER --port=$SMTP_PORT --auth=on --user=$EMAIL_FROM --passwordeval="echo $EMAIL_PASSWORD" --tls=on $EMAIL_TO
         fi
         sleep 10
     done
-    python3 ~/check.py
-    echo "The loading window was checked."
 done
