28bd14af5d0a1ebee01561516378e7fff2fb71c6
tsoichinghin <157337442+tsoichinghin@users.noreply.github.com>
2024-12-01 11:28:50 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-SMTP_SERVER="smtp.office365.com"
-SMTP_PORT=587
-EMAIL_FROM="tsoivm@outlook.com"
-EMAIL_TO="tsoichinghin@gmail.com"
-SUBJECT="$(hostname) PacketShare.exe Terminated"
-
 while true; do
     counter=0
     for i in {1..500}; do
@@ -19,12 +13,7 @@ while true; do
             if [ "$cpu_usage" -gt 30 ]; then
                 echo "CPU usage is $cpu_usage%. Terminating PacketShare.exe."
                 pkill -9 "PacketShare.exe"
-                echo -e "Subject:$SUBJECT\n\nPacketShare.exe was terminated due to high CPU usage ($cpu_usage%)." | \
-                    msmtp --host=$SMTP_SERVER --port=$SMTP_PORT --auth=on --user=$EMAIL_FROM --passwordeval="echo $EMAIL_PASSWORD" --tls=on $EMAIL_TO
             fi
-        else:
-            echo -e "Subject:$SUBJECT\n\nPacketShare.exe was terminated due to high CPU usage ($cpu_usage%)." | \
-                msmtp --host=$SMTP_SERVER --port=$SMTP_PORT --auth=on --user=$EMAIL_FROM --passwordeval="echo $EMAIL_PASSWORD" --tls=on $EMAIL_TO
         fi
         sleep 10
     done
